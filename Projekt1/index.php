de5c92d7580dc648cf2b01b5600f9050c5d122a9
VisikLost <106622121+VisikLost@users.noreply.github.com>
2025-02-19 11:10:21 +0100

    Update index.php

--- Projekt1/index.php
+++ Projekt1/index.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 
-// Check if the user is logged in
 if (!isset($_SESSION['user'])) {
-    // If not logged in, redirect to login page
     header('Location: login.php');
-    exit; // Make sure no further code is executed after the redirect
+    exit;
 }
 ?>
